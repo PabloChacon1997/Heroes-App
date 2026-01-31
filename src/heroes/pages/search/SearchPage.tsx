5cdd702f49c347383ddceebf7c8662dbe1785990
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-01-31 12:17:03 -0500

    working on desing pages

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,17 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <>
+      <CustomJumbotron 
+        title="Busqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos"/>
+      <HeroStats />
+      <SearchControls />
+    </>
   )
 }
+
+export default SearchPage;
